af407dfd6b74fedb8c4939a15c62aae07df0e9b7
rahmadhidayatullah <blessingandguidance@gmail.com>
2020-07-04 22:19:03 +0700

    jhgakdhjfiajihsdf

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
+		<p>Jangkrik</p>
 		<code>application/views/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
